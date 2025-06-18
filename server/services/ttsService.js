84472a147f6d229de772f4a81ab1da49c04bfcdc
RenderHub01 <renderhub01@gmail.com>
2025-06-18 14:58:15 +0530

    done

--- server/services/ttsService.js
+++ server/services/ttsService.js
@@ -8,12 +8,8 @@ dotenv.config();
 const audioDir = path.join(__dirname, "../public/audio");
 
 async function generateAudio(text, filename) {
-  const ELEVENLABS_API_KEY =
-    process.env.ELEVENLABS_API_KEY ||
-    "sk_5bbcefb6cc7d5de05687f11dd353e5da5701673f40b5b2a4";
-  const VOICE_ID = process.env.ELEVENLABS_VOICE_ID || "Xb7hH8MSUJpSbSDYk0k2";
-  const MODEL_ID = "eleven_multilingual_v2";
-  const OUTPUT_FORMAT = "mp3_44100_128"; 
+  const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
+  console.log("ELEVENLABS_API_KEY", ELEVENLABS_API_KEY);
 
   try {
     const audioPath = path.join(audioDir, filename);
@@ -23,8 +19,9 @@ async function generateAudio(text, filename) {
       console.log("✅ Audio directory created");
     }
 
+    // ElevenLabs API call
     const response = await fetch(
-      `https://api.us.elevenlabs.io/v1/text-to-speech/${VOICE_ID}/stream?output_format=${OUTPUT_FORMAT}`,
+      "https://api.elevenlabs.io/v1/text-to-speech/9BWtsMINqrJLrRacOk9x",
       {
         method: "POST",
         headers: {
@@ -34,7 +31,7 @@ async function generateAudio(text, filename) {
         },
         body: JSON.stringify({
           text,
-          model_id: MODEL_ID,
+          model_id: "eleven_monolingual_v1",
           voice_settings: {
             stability: 0.5,
             similarity_boost: 0.5,
@@ -44,8 +41,7 @@ async function generateAudio(text, filename) {
     );
 
     if (!response.ok) {
-      const errorDetails = await response.json();
-      throw new Error(`ElevenLabs API error: ${errorDetails.detail.message}`);
+      throw new Error(`ElevenLabs API error: ${response.statusText}`);
     }
 
     const audioBuffer = await response.arrayBuffer();
